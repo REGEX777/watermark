5670825c7a9303e4c97eb2ee0690091271a5813b
REGEX777 <hmmho705@gmail.com>
2024-08-29 22:16:15 +0530

    tried fixing the dragging logo around

--- public/js/watermark.js
+++ public/js/watermark.js
@@ -3,60 +3,37 @@ const watermarkImage = document.getElementById('watermarkImage');
 const watermarkX = document.getElementById('watermarkX');
 const watermarkY = document.getElementById('watermarkY');
 const imageContainer = document.getElementById('imageContainer');
-const regularImage = document.getElementById('regularImage');
 
-// absolute positioning for the watermark image
-watermarkImage.style.position = 'absolute';
+function setPosition(position) {
+    const containerWidth = imageContainer.clientWidth;
+    const containerHeight = imageContainer.clientHeight;
+    const watermarkWidth = watermarkImage.clientWidth;
+    const watermarkHeight = watermarkImage.clientHeight;
 
-// default position will be (0, 0)
-watermarkImage.style.left = '0px';
-watermarkImage.style.top = '0px';
-watermarkX.value = 0;
-watermarkY.value = 0;
+    let newLeft, newTop;
 
-let isDragging = false;
-
-watermarkImage.onmousedown = function (event) {
-    event.preventDefault();
-    isDragging = true;
-
-    let shiftX = event.clientX - watermarkImage.getBoundingClientRect().left;
-    let shiftY = event.clientY - watermarkImage.getBoundingClientRect().top;
-
-    function moveAt(pageX, pageY) {
-        if (!isDragging) return;
-
-        let newLeft = pageX - shiftX - imageContainer.getBoundingClientRect().left;
-        let newTop = pageY - shiftY - imageContainer.getBoundingClientRect().top;
-
-        // this will ensure that the watermark logo stays in the container
-        if (newLeft < 0) newLeft = 0;
-        if (newTop < 0) newTop = 0;
-        if (newLeft + watermarkImage.clientWidth > regularImage.clientWidth)
-            newLeft = regularImage.clientWidth - watermarkImage.clientWidth;
-        if (newTop + watermarkImage.clientHeight > regularImage.clientHeight)
-            newTop = regularImage.clientHeight - watermarkImage.clientHeight;
-
-        watermarkImage.style.left = `${newLeft}px`;
-        watermarkImage.style.top = `${newTop}px`;
-
-        watermarkX.value = newLeft;
-        watermarkY.value = newTop;
+    switch (position) {
+        case 'top-left':
+            newLeft = 0;
+            newTop = 0;
+            break;
+        case 'top-right':
+            newLeft = containerWidth - watermarkWidth;
+            newTop = 0;
+            break;
+        case 'bottom-left':
+            newLeft = 0;
+            newTop = containerHeight - watermarkHeight;
+            break;
+        case 'bottom-right':
+            newLeft = containerWidth - watermarkWidth;
+            newTop = containerHeight - watermarkHeight;
+            break;
     }
 
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-    }
+    watermarkImage.style.left = `${newLeft}px`;
+    watermarkImage.style.top = `${newTop}px`;
 
-    document.addEventListener('mousemove', onMouseMove);
-
-    watermarkImage.onmouseup = function () {
-        isDragging = false;
-        document.removeEventListener('mousemove', onMouseMove);
-        watermarkImage.onmouseup = null;
-    };
-};
-
-watermarkImage.ondragstart = function () {
-    return false;
-};
+    watermarkX.value = newLeft;
+    watermarkY.value = newTop;
+}
