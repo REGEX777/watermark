e9ab5f2a5173d46d5465355054a2a1f21487c7b9
REGEX777 <hmmho705@gmail.com>
2024-08-29 20:11:36 +0530

    image can now only be moved in the base image size

--- public/js/watermark.js
+++ public/js/watermark.js
@@ -2,12 +2,13 @@
 const watermarkImage = document.getElementById('watermarkImage');
 const watermarkX = document.getElementById('watermarkX');
 const watermarkY = document.getElementById('watermarkY');
+const imageContainer = document.getElementById('imageContainer');
+const regularImage = document.getElementById('regularImage');
 
-
-// absolute positong for the watermark image
+// absolute positioning for the watermark image
 watermarkImage.style.position = 'absolute';
 
-// default posiiton will be (0, 0)
+// default position will be (0, 0)
 watermarkImage.style.left = '0px';
 watermarkImage.style.top = '0px';
 watermarkX.value = 0;
@@ -20,11 +21,22 @@ watermarkImage.onmousedown = function (event) {
     let shiftY = event.clientY - watermarkImage.getBoundingClientRect().top;
 
     function moveAt(pageX, pageY) {
-        watermarkImage.style.left = `${pageX - shiftX}px`;
-        watermarkImage.style.top = `${pageY - shiftY}px`;
+        let newLeft = pageX - shiftX - imageContainer.getBoundingClientRect().left;
+        let newTop = pageY - shiftY - imageContainer.getBoundingClientRect().top;
 
-        watermarkX.value = parseInt(watermarkImage.style.left, 10) || 0;
-        watermarkY.value = parseInt(watermarkImage.style.top, 10) || 0;
+        // Ensure the watermark stays within the container
+        if (newLeft < 0) newLeft = 0;
+        if (newTop < 0) newTop = 0;
+        if (newLeft + watermarkImage.clientWidth > regularImage.clientWidth)
+            newLeft = regularImage.clientWidth - watermarkImage.clientWidth;
+        if (newTop + watermarkImage.clientHeight > regularImage.clientHeight)
+            newTop = regularImage.clientHeight - watermarkImage.clientHeight;
+
+        watermarkImage.style.left = `${newLeft}px`;
+        watermarkImage.style.top = `${newTop}px`;
+
+        watermarkX.value = newLeft;
+        watermarkY.value = newTop;
     }
 
     function onMouseMove(event) {
