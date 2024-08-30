759b24ffc941345454066e510d13ff8208b115a5
REGEX777 <hmmho705@gmail.com>
2024-08-30 14:58:36 +0530

    image export finally works!

--- routes/editor.js
+++ routes/editor.js
@@ -36,8 +36,10 @@ router.post('/upload', async (req, res) => {
 
         await fs.mkdir(imageDir, { recursive: true });
 
-        await regularImage.mv(regularImagePath);
-        await watermarkImage.mv(watermarkImagePath);
+        await Promise.all([
+            regularImage.mv(regularImagePath),
+            watermarkImage.mv(watermarkImagePath)
+        ]);
 
         res.redirect(`/editor?regularImage=${regularImage.name}&watermarkImage=${watermarkImage.name}`);
     } catch (err) {
@@ -50,7 +52,7 @@ router.post('/export', async (req, res) => {
     const { regularImage, watermarkImage, position } = req.body;
 
     if (!regularImage || !watermarkImage || !position) {
-        return res.status(400).send('invalid data provided');
+        return res.status(400).send('Invalid data provided');
     }
 
     const regularImagePath = path.join(__dirname, '../uploads', regularImage);
@@ -58,34 +60,44 @@ router.post('/export', async (req, res) => {
     const outputDir = path.join(__dirname, '../exports');
     const outputImagePath = path.join(outputDir, `exported_${Date.now()}.png`);
 
-    let top, left;
-    switch (position) {
-        case 'top-left':
-            top = 0;
-            left = 0;
-            break;
-        case 'top-right':
-            top = 0;
-            left = await getImageWidth(regularImagePath) - await getImageWidth(watermarkImagePath);
-            break;
-        case 'bottom-left':
-            top = await getImageHeight(regularImagePath) - await getImageHeight(watermarkImagePath);
-            left = 0;
-            break;
-        case 'bottom-right':
-            top = await getImageHeight(regularImagePath) - await getImageHeight(watermarkImagePath);
-            left = await getImageWidth(regularImagePath) - await getImageWidth(watermarkImagePath);
-            break;
-    }
-
     try {
         await fs.mkdir(outputDir, { recursive: true });
+
+        const [regularImageMetadata, watermarkImageMetadata] = await Promise.all([
+            sharp(regularImagePath).metadata(),
+            sharp(watermarkImagePath).metadata()
+        ]);
+
+        let top, left;
+        const margin = 10;
+
+        switch (position) {
+            case 'top-left':
+                top = margin;
+                left = margin;
+                break;
+            case 'top-right':
+                top = margin;
+                left = regularImageMetadata.width - watermarkImageMetadata.width - margin;
+                break;
+            case 'bottom-left':
+                top = regularImageMetadata.height - watermarkImageMetadata.height - margin;
+                left = margin;
+                break;
+            case 'bottom-right':
+                top = regularImageMetadata.height - watermarkImageMetadata.height - margin;
+                left = regularImageMetadata.width - watermarkImageMetadata.width - margin;
+                break;
+            default: // center
+                top = Math.round((regularImageMetadata.height - watermarkImageMetadata.height) / 2);
+                left = Math.round((regularImageMetadata.width - watermarkImageMetadata.width) / 2);
+        }
 
         await sharp(regularImagePath)
             .composite([{
                 input: watermarkImagePath,
-                top: parseInt(top, 10),
-                left: parseInt(left, 10),
+                top: top,
+                left: left,
                 blend: 'overlay',
                 opacity: 0.75
             }])
@@ -93,33 +105,22 @@ router.post('/export', async (req, res) => {
 
         res.download(outputImagePath, async (err) => {
             if (err) {
-                console.error('error exporting image:', err);
-                return res.status(500).send('error exporting image');
+                console.error('Error exporting image:', err);
+                return res.status(500).send('Error exporting image');
             }
 
-            try { //yeet the files out of the machine
+            try {
                 await fs.unlink(outputImagePath);
                 await fs.unlink(regularImagePath);
                 await fs.unlink(watermarkImagePath);
             } catch (cleanupErr) {
-                console.error('error cleaning up files:', cleanupErr);
+                console.error('Error cleaning up files:', cleanupErr);
             }
         });
     } catch (err) {
-        console.error('image processing error', err);
-        res.status(500).send('image processing error');
+        console.error('Image processing error', err);
+        res.status(500).send('Image processing error');
     }
 });
-
-async function getImageWidth(imagePath) {
-    const { width } = await sharp(imagePath).metadata();
-    return width;
-}
-
-async function getImageHeight(imagePath) {
-    const { height } = await sharp(imagePath).metadata();
-    return height;
-}
-
 
 export default router;
